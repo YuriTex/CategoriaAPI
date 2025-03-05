4c720561887377aa2a83312b2298871fd7341a56
YuriTex <yuriteixeira101@gmail.com>
2025-03-05 18:08:55 -0300

    Update CategoriaController.java

--- src/main/java/com/br/demo/controller/CategoriaController.java
+++ src/main/java/com/br/demo/controller/CategoriaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categoria")
+@RequestMapping("/categorias")
 public class CategoriaController {
 
 private final CategoriaService categoriaService;
